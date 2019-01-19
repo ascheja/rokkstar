527fd27fc31f93d6894ff060af426182df8da981
Andreas Scheja <asc@sinceat.net>
2019-01-19 18:28:16 +0100

    Fix equality and string representation of statements

--- ast/src/main/kotlin/net/ascheja/rockstar/ast/statements/BreakStatement.kt
+++ ast/src/main/kotlin/net/ascheja/rockstar/ast/statements/BreakStatement.kt
@@ -2,4 +2,14 @@ package net.ascheja.rockstar.ast.statements
 
 import net.ascheja.rockstar.ast.Statement
 
-class BreakStatement: Statement
+class BreakStatement: Statement {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is BreakStatement) return false
+        return true
+    }
+
+    override fun hashCode(): Int = 0
+
+    override fun toString(): String = "BreakStatement()"
+}
--- ast/src/main/kotlin/net/ascheja/rockstar/ast/statements/ContinueStatement.kt
+++ ast/src/main/kotlin/net/ascheja/rockstar/ast/statements/ContinueStatement.kt
@@ -2,4 +2,14 @@ package net.ascheja.rockstar.ast.statements
 
 import net.ascheja.rockstar.ast.Statement
 
-class ContinueStatement: Statement
+class ContinueStatement: Statement {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ContinueStatement) return false
+        return true
+    }
+
+    override fun hashCode(): Int = 0
+
+    override fun toString(): String = "ContinueStatement()"
+}
