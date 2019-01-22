e884a67a81c9fe98092d678b6bee7189c0eaf0fd
Andreas Scheja <asc@sinceat.net>
2019-01-22 01:43:43 +0100

    Add tests for expressions

---
+++ parser/src/test/kotlin/net/ascheja/rokkstar/parser/ExpressionParserTest.kt
@@ -0,0 +1,249 @@
+package net.ascheja.rokkstar.parser
+
+import net.ascheja.rokkstar.ast.expressions.*
+import net.ascheja.rokkstar.ast.expressions.BinaryOperatorExpression.Operator.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExpressionParserTest {
+
+    @Test
+    fun `mysterious assignment parsed correctly`() {
+        assertEquals(
+            UndefinedLiteralExpression(),
+            createParser("mysterious").parseExpression()
+        )
+    }
+
+    @Test
+    fun `true assignment parsed correctly`() {
+        for (keyword in setOf("true", "right", "yes", "ok")) {
+            assertEquals(
+                BooleanLiteralExpression(true),
+                createParser(keyword).parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `false assignment parsed correctly`() {
+        for (keyword in setOf("false", "wrong", "no", "lies")) {
+            assertEquals(
+                BooleanLiteralExpression(false),
+                createParser(keyword).parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `null assignment parsed correctly`() {
+        for (keyword in setOf("null", "nothing", "nowhere", "nobody", "empty", "gone")) {
+            assertEquals(NullLiteralExpression(), createParser(keyword).parseExpression())
+        }
+    }
+
+    @Test
+    fun `string assignment parsed correctly`() {
+        assertEquals(
+            StringLiteralExpression("some text rockstar doesn't give a shit about"),
+            createParser("\"some text rockstar doesn't give a shit about\"").parseExpression()
+        )
+    }
+
+    @Test
+    fun `number assignment parsed correctly`() {
+        for ((input, expectedValue) in listOf("2" to 2.0, "0" to 0.0, "0.273" to 0.273, "10.9" to 10.9)) {
+            assertEquals(
+                NumberLiteralExpression(expectedValue),
+                createParser(input).parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `and parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            AND,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        assertEquals(
+            expected,
+            createParser("true and true").parseExpression()
+        )
+    }
+
+    @Test
+    fun `or parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            OR,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        assertEquals(
+            expected,
+            createParser("true or true").parseExpression()
+        )
+    }
+
+    @Test
+    fun `nor parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            NOR,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        assertEquals(
+            expected,
+            createParser("true nor true").parseExpression()
+        )
+    }
+
+    @Test
+    fun `equal parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            EQUALS,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        assertEquals(
+            expected,
+            createParser("true is true").parseExpression()
+        )
+    }
+
+    @Test
+    fun `not equal parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            NOT_EQUALS,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("ain't", "isn't")) {
+            assertEquals(
+                expected,
+                createParser("true $keyword true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `greater parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            GREATER,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("higher", "bigger", "greater", "stronger")) {
+            assertEquals(
+                expected,
+                createParser("true is $keyword than true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `greater equals parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            GREATER_EQUALS,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("high", "big", "great", "strong")) {
+            assertEquals(
+                expected,
+                createParser("true is as $keyword as true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `less parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            LESS,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("less", "lower", "smaller", "weaker")) {
+            assertEquals(
+                expected,
+                createParser("true is $keyword than true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `less equals parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            LESS_EQUALS,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("low", "little", "small", "weak")) {
+            assertEquals(
+                expected,
+                createParser("true is as $keyword as true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `plus,with parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            ADD,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("plus", "with")) {
+            assertEquals(
+                expected,
+                createParser("true $keyword true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `minus,without parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            SUBTRACT,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("minus", "without")) {
+            assertEquals(
+                expected,
+                createParser("true $keyword true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `times,of parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            MULTIPLY,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        for (keyword in setOf("times", "of")) {
+            assertEquals(
+                expected,
+                createParser("true $keyword true").parseExpression()
+            )
+        }
+    }
+
+    @Test
+    fun `over parsed correctly`() {
+        val expected = BinaryOperatorExpression(
+            DIVIDE,
+            BooleanLiteralExpression(true),
+            BooleanLiteralExpression(true)
+        )
+        assertEquals(
+            expected,
+            createParser("true over true").parseExpression()
+        )
+    }
+
+    private fun createParser(text: String): ExpressionParser = ExpressionParser(Lexer(text).tokens)
+}
